5ff7c7ef5285f7fbda606070c042546ec1d39175
chenghjilai <chengjilai@sjtu.edu.cn>
2025-10-14 12:10:15 +0800

    fix: correct version number

--- src/jdisk/__init__.py
+++ src/jdisk/__init__.py
@@ -3,7 +3,7 @@
 A simple Python implementation for Shanghai Jiao Tong University Netdisk client.
 """
 
-__version__ = "0.1.9"
+__version__ = "0.2.0"
 __author__ = "chengjilai"
 
 from .auth import SJTUAuth
